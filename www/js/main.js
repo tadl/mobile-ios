2078a9a03a1db237f2cfc444d7db2b1cb40f66d8
Jeff Godin <jgodin@tadl.org>
2013-08-16 17:13:31 -0400

    Include mobi_bypass argument in JSON urls
    Work around some situations where (due to user agent) we were being
    redirected to an HTML page instead of receiving the JSON data that
    we requested.
    
    This manifested itself in Featured / Locations / Events not working
    the first time one was selected, but then all further attempts to
    load the data (during a single run of the app) were successful.
    
    The mobi_bypass argument ensures that our current method of mobile
    redirection does not fire on the server side.
    
    Signed-off-by: Jeff Godin <jgodin@tadl.org>

--- www/js/main.js
+++ www/js/main.js
@@ -1,9 +1,9 @@
 var ILSCATCHER_HOST = 'tadl-ilscatcher.herokuapp.com'
 var ILSCATCHER_BASE = 'https://' + ILSCATCHER_HOST
 var ILSCATCHER_INSECURE_BASE = 'https://' + ILSCATCHER_HOST /* we will actually use https here also */
-var FEATURED_URL = 'https://www.tadl.org/mobile/export/items/json/featured'
-var EVENTS_URL = 'https://www.tadl.org/mobile/export/events/json/all'
-var LOCATIONS_BASE = 'https://www.tadl.org/mobile/export/locations'
+var FEATURED_URL = 'https://www.tadl.org/mobile/export/items/json/featured?mobi_bypass=1'
+var EVENTS_URL = 'https://www.tadl.org/mobile/export/events/json/all?mobi_bypass=1'
+var LOCATIONS_BASE = 'https://www.tadl.org/mobile/export/locations?mobi_bypass=1'
 var PLACEHOLDER_IMG = 'img/clocktower100.png';
 var FACEBOOK_URL = 'https://graph.facebook.com/TraverseAreaDistrictLibrary/feed?access_token=CAAFh5Quq0YMBAENgjPYY9MY0y3cdiAMvXmLl6Fq3H4LDZBBZBukKlXFwWPq0xMLa6hqDrfxfGqvFpBlIZCjFKg0rKdd37qHLsjwcBve4UeZAQymPksV7ddAeZAJOyaeZC05WqlLzrVpOUQEtjiCZArjB6NMUHjvU90qXZAGEOESKDgZDZD';
 var loadingmoreText = '<span class="loadmore"><img style="margin-right: 10px; margin-left: 10px;" src="img/ajax-loader-2.gif">LOADING...</span>';
